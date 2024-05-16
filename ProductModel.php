97f5db9c923ce9d24efbe432a2f2a693ebc5e3e1
kodoku-san <kodoku169@gmail.com>
2024-05-16 22:34:28 +0700

    #13 @kodoku-san - create ProductModel.php

---
+++ ProductModel.php
@@ -0,0 +1,12 @@
+<?php
+
+class ProductModel {
+
+    public function __construct() {
+        echo 'ProductModel';
+    }
+
+    public function getProduct() {
+        return 'Product update';
+    }
+}
